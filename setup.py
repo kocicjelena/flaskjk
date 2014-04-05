6cf41ac1bff76e0095ae9f2d01ac69ed71f3c870
Jelena Kocic <kocicjelena@gmail.com>
2014-04-05 16:07:36 +0200

    1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='flaskjk',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Jelena',
+      author_email='kocicjelena@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask==0.7.2', 'MarkupSafe' , 'Flask-SQLAlchemy==0.16'],
      )
